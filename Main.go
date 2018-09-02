e80dcc513c6924a26db378aab888aaeebcb46401
Jacek Musiał <github@musi.al>
2018-09-02 16:23:43 +0200

    Add language column

--- Main.go
+++ Main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"github.com/go-sql-driver/mysql"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gocolly/colly"
 	"github.com/rakanalh/scheduler"
@@ -49,20 +50,15 @@ func main() {
 	checkErr(err)
 	memStorage := storage.NewMemoryStorage()
 	s := scheduler.New(memStorage)
-
 	fmt.Println("Im good at school three huna worldwide")
-
-	stmt, err := db.Prepare("INSERT INTO IE (fullurl, codedate, codekey, size, codelines, language, " +
-		"status, txt) VALUES (?,?,?,?,?,?,?,?)")
-
-	if _, err := s.RunEvery(30 * time.Second, scrapIdeone, db, stmt); err != nil {
+	if _, err := s.RunEvery(40 * time.Second, scrapIdeone, db); err != nil {
 		log.Fatal(err)
 	}
 	s.Start()
 	s.Wait()
 }
 
-func scrapIdeone(db *sql.DB, stmt *sql.Stmt) {
+func scrapIdeone(db *sql.DB) {
 	links, result := getRecentLinks()
 	//jdbc:mariadb://192.168.1.65:3306/ideone
 
@@ -82,19 +78,31 @@ func scrapIdeone(db *sql.DB, stmt *sql.Stmt) {
 		} else {
 			defer response.Body.Close()
 			html, err := ioutil.ReadAll(response.Body)
-			var txt string = string(html)
-			fmt.Println(len(html))
+			print(response.Header.Get("Content-Disposition"))
+			get := response.Header.Get("Content-Disposition")
+			fmt.Println("get: ", get)
+			start := strings.Index(get, "=\"")
+			fmt.Println(start)
+			filename := get[(start + 1):]
+			language := filename[strings.Index(filename, ".")+1 : len(filename)-1]
 
+			var txt = string(html)
+			fmt.Println(len(html))
+			stmt, err := db.Prepare("INSERT INTO IE (fullurl, codedate, codekey, size, codelines, language, " +
+				"status, txt) VALUES (?,?,?,?,?,?,?,?)")
 			checkErr(err)
 
 			currentTime := time.Now()
-			res, err := stmt.Exec(url, currentTime.Format("2006-01-02 15:04:05"), k[1:], len(html),
-				strings.Count(txt, "\n"), "language", results[i], txt)
-			if res == nil {
-
+			_, err = stmt.Exec(url, currentTime.Format("2006-01-02 15:04:05"), k[1:], len(html),
+				strings.Count(txt, "\n"), language, results[i], txt)
+			if err != nil {
+				me, _ := err.(*mysql.MySQLError)
+				if me.Number == 1062 {
+					fmt.Println("It already exists in a database. ", err)
+				} else {
+					checkErr(err)
+				}
 			}
-			checkErr(err)
-
 		}
 
 		fmt.Println(result[i], ", ", url)
